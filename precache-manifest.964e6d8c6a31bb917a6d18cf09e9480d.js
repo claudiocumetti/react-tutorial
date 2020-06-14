d69a3a9c5e291975f6b6b5e21a9e99842347702e
Claudio Cumetti <claudio_cumetti@hotmail.com>
2020-06-14 05:54:52 -0400

    Updates

---
+++ precache-manifest.964e6d8c6a31bb917a6d18cf09e9480d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a8849c0897f49269e7eebdcefaadc7",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a32cf54b36ce45dd532a",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "016172e848e1ad467c3c",
+    "url": "/react-tutorial/static/js/2.ca98edaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.ca98edaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32cf54b36ce45dd532a",
+    "url": "/react-tutorial/static/js/main.0904e0db.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
